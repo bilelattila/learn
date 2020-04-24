b9508ead9dfe8375d0dd4467e4bb88b9b600c149
macbook <macbook@MacBook-Pro-de-C02S11Y3FVH7.local>
2020-04-24 16:18:09 +0100

    ok

--- decisiontree.py
+++ decisiontree.py
@@ -5,8 +5,8 @@ import sklearn.tree
 from sklearn.datasets import load_iris
 from  sklearn.model_selection import train_test_split
 iris=load_iris()
-
-X_train, X_test, Y_train, Y_test=train_test_split(iris.data, iris.target, test_size=0.3)
+print(iris.data[2])
+X_train, X_test, Y_train, Y_test= train_test_split(iris.data, iris.target, test_size=0.3)
 print(X_train,Y_train)
 clf=sklearn.tree.DecisionTreeClassifier()
 clf.fit(X_train, Y_train)
